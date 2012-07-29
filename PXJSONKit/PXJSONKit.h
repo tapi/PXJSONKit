328e164af5a1ab1e4b24a533c5a300daa6d6f0b8
Paddy O'Brien <tapi@simplemind.ca>
2012-07-28 21:32:25 -0400

    Profiling done

--- PXJSONKit/PXJSONKit.h
+++ PXJSONKit/PXJSONKit.h
@@ -3,11 +3,36 @@
 //  PXJSONKit
 //
 //  Created by Paddy O'Brien on 12-07-28.
-//  Copyright (c) 2012 Paddy O'Brien. All rights reserved.
+//  Copyright (c) 2012 500px. All rights reserved.
 //
+//  Used to evaluate various JSON Libraries. Th e need to define a common interface obviously leads to th loss of some features.
+
+#define PX_PROFILING		0
+
+#define PX_JSON_NATIVE		1	//Requires iOS5 or later
+#define PX_JSON_JSONKIT		2
+#define PX_JSON_SBJSON		3
+
+#define PX_JSON_STRATEGY	PX_JSON_NATIVE
+
 
 #import <Foundation/Foundation.h>
 
-@interface PXJSONKit : NSObject
+@interface NSArray (Serialization)
+- (NSData *)PX_dataWithJSONObject:(id)obj error:(NSError **)error;
+@end
 
+@interface NSDictionary (Serialization)
+- (NSData *)PX_dataWithJSONObject:(id)obj error:(NSError **)error;
+@end
+
+
+@interface NSData (Deserialization)
+- (id)PX_objectWithJSONDataError:(NSError **)error;
+- (id)PX_mutablObjectWithJSONDataError:(NSError **)error;
+@end
+
+@interface NSString (Deserialization)
+- (id)PX_objectWithJSONStringError:(NSError **)error;
+- (id)PX_mutablObjectWithJSONStringError:(NSError **)error;
 @end
--- PXJSONKit/PXJSONKit.m
+++ PXJSONKit/PXJSONKit.m
@@ -3,11 +3,130 @@
 //  PXJSONKit
 //
 //  Created by Paddy O'Brien on 12-07-28.
-//  Copyright (c) 2012 Paddy O'Brien. All rights reserved.
+//  Copyright (c) 2012 500px. All rights reserved.
 //
 
 #import "PXJSONKit.h"
+#import <mach/mach.h>
+#import <mach/mach_time.h>
 
-@implementation PXJSONKit
+#if PX_JSON_STRATEGY == PX_JSON_JSONKIT
+#import "JSONKit.h"
+#elif PX_JSON_STRATEGY == PX_JSON_SBJSON
+#import "SBJson.h"
+#endif
 
+// Profiling
+void PXComputeDelta(uint64_t start, uint64_t end, const char *methodName)
+{
+	uint64_t elapsed = end - start;
+	uint64_t elapsedNano;
+	static mach_timebase_info_data_t    sTimebaseInfo;
+	
+    if ( sTimebaseInfo.denom == 0 ) {
+        (void) mach_timebase_info(&sTimebaseInfo);
+    }
+	
+    elapsedNano = elapsed * sTimebaseInfo.numer / sTimebaseInfo.denom;
+	
+#if PX_JSON_STRATEGY == PX_JSON_NATIVE
+#elif PX_JSON_STRATEGY == PX_JSON_JSONKIT
+#elif PX_JSON_STRATEGY == PX_JSON_SBJSON
+#endif
+}
+
+#if PX_PROFILING
+#define PX_PROFILE_START uint64_t start = mach_absolute_time();
+#else
+#define PX_PROFILE_START
+#endif
+
+#if PX_PROFILING
+#define PX_PROFILE_END uint64_t end = mach_absolute_time(); PXComputeDelta(start, end, __PRETTY_FUNCTION__);
+#else
+#define PX_PROFILE_END
+#endif
+
+
+@implementation NSArray (Serialization)
+- (NSData *)PX_dataWithJSONObject:(id)obj error:(NSError **)error
+{
+	NSData *JSONData;
+	
+	PX_PROFILE_START
+	
+#if PX_JSON_STRATEGY == PX_JSON_NATIVE
+#elif PX_JSON_STRATEGY == PX_JSON_JSONKIT
+#elif PX_JSON_STRATEGY == PX_JSON_SBJSON
+#endif
+	
+	PX_PROFILE_END
+	
+	return JSONData;
+}
+@end
+
+@implementation NSDictionary (Serialization)
+- (NSData *)PX_dataWithJSONObject:(id)obj error:(NSError **)error
+{
+	NSData *JSONData;
+	
+	PX_PROFILE_START
+	
+#if PX_JSON_STRATEGY == PX_JSON_NATIVE
+#elif PX_JSON_STRATEGY == PX_JSON_JSONKIT
+#elif PX_JSON_STRATEGY == PX_JSON_SBJSON
+#endif
+	
+	PX_PROFILE_END
+	
+	return JSONData;
+}
+@end
+
+
+@implementation NSData (Deserialization)
+- (id)PX_objectWithJSONDataError:(NSError **)error
+{
+	PX_PROFILE_START
+	
+#if PX_JSON_STRATEGY == PX_JSON_NATIVE
+#elif PX_JSON_STRATEGY == PX_JSON_JSONKIT
+#elif PX_JSON_STRATEGY == PX_JSON_SBJSON
+#endif
+	
+	PX_PROFILE_END
+}
+
+- (id)PX_mutablObjectWithJSONDataError:(NSError **)error
+{
+	PX_PROFILE_START
+#if PX_JSON_STRATEGY == PX_JSON_NATIVE
+#elif PX_JSON_STRATEGY == PX_JSON_JSONKIT
+#elif PX_JSON_STRATEGY == PX_JSON_SBJSON
+#endif
+	PX_PROFILE_END
+}
+@end
+
+@implementation NSString (Deserialization)
+- (id)PX_objectWithJSONStringError:(NSError **)error
+{
+	PX_PROFILE_START
+#if PX_JSON_STRATEGY == PX_JSON_NATIVE
+#elif PX_JSON_STRATEGY == PX_JSON_JSONKIT
+#elif PX_JSON_STRATEGY == PX_JSON_SBJSON
+#endif
+	PX_PROFILE_END
+}
+
+- (id)PX_mutablObjectWithJSONStringError:(NSError **)error
+{
+	PX_PROFILE_START
+#if PX_JSON_STRATEGY == PX_JSON_NATIVE
+#elif PX_JSON_STRATEGY == PX_JSON_JSONKIT
+#elif PX_JSON_STRATEGY == PX_JSON_SBJSON
+#endif
+	PX_PROFILE_END
+}
 @end
